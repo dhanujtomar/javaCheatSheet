61a047345c6b2ae7c60db37cb18f6d267a2367e6
dhanujtomar <dhanujtomar@gmail.com>
2022-10-02 22:08:54 +0530

    Final Commit

--- Exp2.java
+++ Exp2.java
@@ -12,29 +12,31 @@ class VideoStore {
     Video beat[] = new Video[10];
     int num_video;
 
-void addVideo() {
-System.out.println("Enter " + num_video + " Video Title:- ");
-for (int i = 0; i < num_video; i++) {
-beat[i] = new Video();
-beat[i].title = input.nextLine();
-}
-System.out.println("Enter " + num_video + " Video rating between 1 to 5:- ");
-for(int i = 0; i < num_video; i++) {
-beat[i].avg= input.nextInt();
-}
-}
+    void addVideo() {
+        System.out.println("Enter " + num_video + " Video Title:- ");
+        for (int i = 0; i < num_video; i++) {
+            beat[i] = new Video();
+            beat[i].title = input.nextLine();
+        }
+        System.out.println("Enter " + num_video + " Video rating between 1 to 5:- ");
+        for (int i = 0; i < num_video; i++) {
+            beat[i].avg = input.nextInt();
+        }
+    }
 
-int chekOut(int k) {
-String chek1;
-System.out.println("chekout " + (k + 1));
-chek1 = input.next();
-for (int i = 0; i < num_video; i++) {
-if (beat[i].title.equals(chek1) && (beat[i].Flag == false)) {
-beat[i].Flag = true;
-return -1;
-} else if (beat[i].title.equals(chek1) && (beat[i].Flag == true)) {
-System.out.println("Failed to chekout: ");
-return -1;
+    int chekOut(int k) {
+        String chek1;
+        System.out.println("chekout " + (k + 1));
+        chek1 = input.next();
+        for (int i = 0; i < num_video; i++) {
+            if (beat[i].title.equals(chek1) && (beat[i].Flag == false)) {
+                beat[i].Flag = true;
+                return -1;
+            } else if (beat[i].title.equals(chek1) && (beat[i].Flag == true)) {
+                System.out.println("Failed to chekout: ");
+                return -1;
+            }
+        }
+        return 1;
+    }
 }
-}
-return 1;
